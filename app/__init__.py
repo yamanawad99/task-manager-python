7441e8f3c75daeb631ffe0b3a6c492f0ca168715
Yaman Awad <yaman.awad@montymobile.com>
2024-06-24 03:42:49 +0300

    no need to use builder design pattern for initiating app, instead do that for celery to avoid circular imports

--- app/__init__.py
+++ app/__init__.py
@@ -12,20 +12,35 @@ migrate = Migrate()
 mongo_client = MongoClient(Config.MONGO_URI)
 mongo_db = mongo_client.get_default_database()
 
-celery = Celery(__name__, broker=Config.CELERY_BROKER_URL)
+from celery import Celery, Task
 
+def celery_init_app(app: Flask) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.app_context():
+                return self.run(*args, **kwargs)
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(Config)
-    app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
-    db.init_app(app)
-    login_manager.init_app(app)
-    migrate.init_app(app, db)
-    celery.conf.update(app.config)
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
 
-    from app import routes
-    app.register_blueprint(routes.bp)
+app = Flask(__name__)
+app.config.from_mapping(
+    CELERY=dict(
+        broker_url=Config.CELERY_BROKER_URL,
+        result_backend=Config.CELERY_RESULT_BACKEND,
+        task_ignore_result=True,
+    ),
+)
+celery_app = celery_init_app(app)
+app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = Config.SQLALCHEMY_TRACK_MODIFICATIONS
 
-    return app
+db.init_app(app)
+login_manager.init_app(app)
+migrate.init_app(app, db)
+from app import routes
+app.register_blueprint(routes.bp)
