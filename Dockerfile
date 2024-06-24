aa50d6ae639e26c4b1ff9b62572f24c3e698d57d
Yaman Awad <yaman.awad@montymobile.com>
2024-06-24 03:43:21 +0300

    add redis and celery to docker files

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.9
 RUN apt-get update && apt-get install -y postgresql-client && rm -rf /var/lib/apt/lists/*
 WORKDIR /app
 
-COPY ../task-mng-python/requirements.txt .
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
--- config.py
+++ config.py
@@ -8,3 +8,4 @@ class Config:
     MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://mongo:27017/task_manager_logs'
     CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL') or 'redis://redis:6379/0'
     CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND') or 'redis://redis:6379/0'
+
